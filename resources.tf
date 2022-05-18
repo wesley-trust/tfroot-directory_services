5d14ee704751a02343ef0e17b0d421b8eff80deb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-18 21:25:38 +0100

    Update

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
-/* module "directory_services_virtual_machines" {
+module "directory_services_virtual_machines" {
   for_each                = toset(local.resource_locations)
-  source                  = "../Modules/Deployments/Windows_virtual_machine"
+  source                  = "github.com/wesley-trust/tfmodule-windows_virtual_machine"
   service_environment     = terraform.workspace
   service_deployment      = "01"
   service_name            = var.service_name
@@ -15,7 +15,7 @@
 
 module "directory_services_network_peering" {
   for_each                   = toset(local.resource_locations)
-  source                     = "../Modules/Deployments/Network_peering"
+  source                     = "github.com/wesley-trust/tfmodule-network_peering"
   service_environment        = terraform.workspace
   service_deployment         = "01"
   service_name               = var.service_name
@@ -27,7 +27,7 @@ module "directory_services_network_peering" {
 module "directory_services_recovery_services" {
   depends_on                                  = [module.directory_services_virtual_machines]
   for_each                                    = toset(local.resource_recovery_services_locations)
-  source                                      = "../Modules/Deployments/Recovery_services"
+  source                                      = "github.com/wesley-trust/tfmodule-recovery_services"
   service_environment                         = terraform.workspace
   service_deployment                          = "01"
   service_name                                = "${var.service_name}-RSV"
@@ -39,7 +39,7 @@ module "directory_services_recovery_services" {
 
 module "directory_services_virtual_machines_bcdr" {
   for_each                = toset(local.resource_bcdr_locations)
-  source                  = "../Modules/Deployments/Windows_virtual_machine"
+  source                  = "github.com/wesley-trust/tfmodule-windows_virtual_machine"
   service_environment     = terraform.workspace
   service_deployment      = "01"
   service_name            = var.service_name
@@ -54,11 +54,11 @@ module "directory_services_virtual_machines_bcdr" {
 
 module "directory_services_network_peering_bcdr" {
   for_each                   = toset(local.resource_bcdr_locations)
-  source                     = "../Modules/Deployments/Network_peering"
+  source                     = "github.com/wesley-trust/tfmodule-network_peering"
   service_environment        = terraform.workspace
   service_deployment         = "01"
   service_name               = var.service_name
   service_location           = each.value
   resource_network_peer      = module.directory_services_virtual_machines_bcdr[each.value]
   resource_network_peer_role = var.resource_network_peer_role
-} */
+}
