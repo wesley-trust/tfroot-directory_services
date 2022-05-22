b19c99dfe087d7d8f992046c083f655797e0a0f1
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-22 18:10:39 +0100

    Upadte [skip ci]

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-module "directory_services_virtual_machines" {
+/* module "directory_services_virtual_machines" {
   for_each                = toset(local.resource_locations)
   source                  = "github.com/wesley-trust/tfmodule-windows_virtual_machine"
   service_environment     = terraform.workspace
@@ -11,7 +11,7 @@ module "directory_services_virtual_machines" {
   resource_address_space  = lookup(var.resource_address_space, each.value, null)
   resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
   resource_network_role   = var.resource_network_role
-}
+} */
 
 /* module "directory_services_network_peering" {
   for_each                   = toset(local.resource_locations)
@@ -34,7 +34,7 @@ module "directory_services_virtual_machines" {
   resource_recovery_services_virtual_machines = module.directory_services_virtual_machines[each.value]
 } */
 
-module "directory_services_virtual_machines_bcdr" {
+/* module "directory_services_virtual_machines_bcdr" {
   for_each                = toset(local.resource_bcdr_locations)
   source                  = "github.com/wesley-trust/tfmodule-windows_virtual_machine"
   service_environment     = terraform.workspace
@@ -47,7 +47,7 @@ module "directory_services_virtual_machines_bcdr" {
   resource_address_space  = lookup(var.resource_address_space, each.value, null)
   resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
   resource_network_role   = var.resource_network_role
-}
+} */
 
 /* module "directory_services_network_peering_bcdr" {
   for_each                   = toset(local.resource_bcdr_locations)
