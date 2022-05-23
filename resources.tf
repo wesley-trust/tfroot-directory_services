a5b5ce6066e8338b11d1e01373285767e4a6a164
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-23 09:28:15 +0100

    Test deploy

--- resources.tf
+++ resources.tf
@@ -21,7 +21,7 @@ module "directory_services_virtual_machines" {
   resource_network_peer_role = var.resource_network_peer_role
 } */
 
-module "directory_services_recovery_services" {
+/* module "directory_services_recovery_services" {
   depends_on                                  = [module.directory_services_virtual_machines]
   for_each                                    = toset(local.resource_recovery_services_locations)
   source                                      = "github.com/wesley-trust/tfmodule-recovery_services"
@@ -32,9 +32,9 @@ module "directory_services_recovery_services" {
   resource_name                               = local.resource_name
   resource_recovery_services_instance_count   = local.resource_recovery_services_instance_count
   resource_recovery_services_virtual_machines = module.directory_services_virtual_machines[each.value]
-}
+} */
 
-/* module "directory_services_virtual_machines_bcdr" {
+module "directory_services_virtual_machines_bcdr" {
   for_each                = toset(local.resource_bcdr_locations)
   source                  = "github.com/wesley-trust/tfmodule-windows_virtual_machine"
   service_environment     = terraform.workspace
@@ -47,7 +47,7 @@ module "directory_services_recovery_services" {
   resource_address_space  = lookup(var.resource_address_space, each.value, null)
   resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
   resource_network_role   = var.resource_network_role
-} */
+}
 
 /* module "directory_services_network_peering_bcdr" {
   for_each                   = toset(local.resource_bcdr_locations)
